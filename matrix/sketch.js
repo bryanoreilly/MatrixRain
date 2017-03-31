75a18987c3bbbabe290deff400d9a0c8836b596b
SpaceCowboy <bryanoreilly770@gmail.com>
2017-03-30 21:52:40 -0400

    Initial Commit
    Matrix Rain Sketch!

---
+++ matrix/sketch.js
@@ -0,0 +1,82 @@
+var symbolSize = 26;
+var streams = [];
+
+function setup() {
+  createCanvas(
+    window.innerWidth,
+    window.innerHeight
+    );
+    background(0);
+    var x = 0;
+    for(var i = 0; i <= width/symbolSize; i++) {
+     var stream = new Stream();
+     stream.generateSymbols(x, random(-1000, 0));
+     streams.push(stream);
+     x += symbolSize;
+    }
+    textSize(symbolSize);
+}
+
+function draw() {
+  background(0, 180);
+  streams.forEach(function(stream){
+    stream.render();
+  });
+}
+
+function Symbol(x, y, speed, first){
+  this.x = x;
+  this.y = y;
+  this.value;
+  this.speed = speed;
+  this.switchInterval = round(random(2, 20));
+  this.first = first;
+  
+  this.setToRandomSymbol = function(){
+    if(frameCount % this.switchInterval == 0){
+      this.value = String.fromCharCode(
+        0x30A0 + round(random(0, 96))
+      );
+    }
+  }
+  
+  this.rain = function(){
+    // if(this.y >= height){
+    //   this.y = 0;
+    // } else {
+    // this.y += this.speed;
+    // }
+    this.y = (this.y >= height) ? 0 : this.y += this.speed;
+  }
+}
+
+function Stream(){
+  this.symbols = [];
+  this.totalSymbols = round(random(10, 30));
+  this.speed = random(5, 20);
+  
+  this.generateSymbols = function(x, y){
+    var first = round(random(0, 4)) == 1;
+    for(var i = 0; i <= this.totalSymbols; i++){
+      symbol = new Symbol(x, y, this.speed, first);
+      symbol.setToRandomSymbol();
+      this.symbols.push(symbol);
+      y -= symbolSize;
+      first = false;
+    }
+  }
+  
+  this.render = function(){
+    this.symbols.forEach(function(symbol){
+      if(symbol.first){
+        fill(160, 225, 185)
+      } else {
+        fill(20, 150, 113);
+      }
+      //fill(0,220, 70);
+      text(symbol.value, symbol.x, symbol.y);
+      symbol.rain();
+      symbol.setToRandomSymbol();
+    });
+  }
+}
